52ec5c935990c8f6004fe60e8c1be995cec4470d
Gepard45 <45Gepard@gmail.com>
2016-11-18 03:50:19 +0500

    Update s1.py

--- home/box/s1.py
+++ home/box/s1.py
@@ -6,8 +6,8 @@ s.listen(1)
 while True:                                                                     
     client, address = s.accept()
     while True:
-    data = client.recv(1024)
-    if not data: break
-    if data == "Close" or data == "close": client.close()
+      data = client.recv(1024)
+      if not data: break
+      if data == "Close" or data == "close": client.close()
     client.send(data)
 client.close()
