80265e593f516cf3df8adae5ccfddc0a1e9629f0
Gepard45 <45Gepard@gmail.com>
2016-11-18 00:39:54 +0500

    Update s1.py

--- home/box/s1.py
+++ home/box/s1.py
@@ -7,8 +7,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen(1)
     conn, addr = s.accept()
     with conn:
-        print('Connected by', addr)
         while True:
             data = conn.recv(1024)
             if not data: break
+            if data == 'close': break
             conn.sendall(data)
