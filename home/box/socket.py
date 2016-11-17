cc432aa693ff196d4b4c52b79eafaec8d383d200
Gepard45 <45Gepard@gmail.com>
2016-11-18 04:23:48 +0500

    Update socket.py

--- home/box/socket.py
+++ home/box/socket.py
@@ -2,7 +2,7 @@ import socket
                                                                                 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
 s.bind(('0.0.0.0', 2222))                                                     
-s.listen(1)                                                                     
+s.listen(5)                                                                     
 while True:                                                                     
         client, address = s.accept()
         while True:
