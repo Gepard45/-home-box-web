26faef85c26926a10e7291e79f3ff14b39e0d57c
Gepard45 <45Gepard@gmail.com>
2016-11-18 03:19:22 +0500

    Update s1.py

--- home/box/s1.py
+++ home/box/s1.py
@@ -1,14 +1,13 @@
-import socket
-
-HOST = '0.0.0.0'                 # Symbolic name meaning all available interfaces
-PORT = 2222              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            if data == 'close': break
-            conn.sendall(data)
+import socket 
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(1)
+while True:
+	client, address = s.accept()
+	while True:
+		data = client.recv(1024)
+		if not data: break
+		if data == "Close" or data == "close": client.close()
+	client.send(data)
+client.close()
