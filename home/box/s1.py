a39d1bb6d04361664ce289cb89d35b0bbd830856
Gepard45 <45Gepard@gmail.com>
2016-11-18 15:59:32 +0500

    Update s1.py

--- home/box/s1.py
+++ home/box/s1.py
@@ -1,5 +1,4 @@
-!/bin/bash import socket
-#import socket                                                                   
+import socket                                                                   
                                                                                 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                           
 s.bind(('0.0.0.0', 2222))                                                     
